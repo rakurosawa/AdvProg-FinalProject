cc5625cefa7ac2b034d7965fc4fd63d0aca39497
astewardnolan <123675867+astewardnolan@users.noreply.github.com>
2024-12-17 23:25:48 -0500

    sml edits

--- radixSort.c
+++ radixSort.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-// A utility function to get the maximum value in arr[]
 int getMax(int arr[], int n) {
     int mx = arr[0];
     for (int i = 1; i < n; i++)
@@ -24,17 +23,15 @@ void countSort(int arr[], int n, int exp) {
     for (int i = 1; i < 10; i++)
         count[i] += count[i - 1];
 
-    // Build the output array
     for (int i = n - 1; i >= 0; i--) {
         output[count[(arr[i] / exp) % 10] - 1] = arr[i];
         count[(arr[i] / exp) % 10]--;
     }
 
-    // Copy the output array to arr[], so that arr[] now contains sorted numbers according to current digit
     for (int i = 0; i < n; i++)
         arr[i] = output[i];
 
-    free(output); // Free the dynamically allocated memory
+    free(output); 
 }
 
 // The main function to sort arr[] of size n using Radix Sort
